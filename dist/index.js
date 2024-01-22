1650a3db29bb7f9b73c06afecb070ab45484e312
WooSng <cwstbp0426@gmail.com>
2024-01-23 00:54:27 +0900

    vercel용 설정 변경

--- dist/index.js
+++ dist/index.js
@@ -1,6 +1,7 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 const tslib_1 = require("tslib");
+require("module-alias/register");
 const express_1 = tslib_1.__importDefault(require("express"));
 const dotenv_1 = tslib_1.__importDefault(require("dotenv"));
 const helmet_1 = tslib_1.__importDefault(require("helmet"));
