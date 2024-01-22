9b721645149d324bf1f008d7fc5865680e1edd3e
WooSng <cwstbp0426@gmail.com>
2024-01-23 01:38:43 +0900

    cors 변경

--- dist/index.js
+++ dist/index.js
@@ -1,15 +1,17 @@
 "use strict";
+var _a;
 Object.defineProperty(exports, "__esModule", { value: true });
 const tslib_1 = require("tslib");
 // import "module-alias/register";
 const express_1 = tslib_1.__importDefault(require("express"));
 const dotenv_1 = tslib_1.__importDefault(require("dotenv"));
 const helmet_1 = tslib_1.__importDefault(require("helmet"));
+const cors_1 = tslib_1.__importDefault(require("cors"));
 dotenv_1.default.config();
 if (process.env.NODE_ENV === "production") {
     require("module-alias/register");
 }
-const cors_1 = tslib_1.__importDefault(require("@/middleware/cors"));
+// import cors from "@/middleware/cors";
 const config_1 = tslib_1.__importDefault(require("@/lib/mongodb/config"));
 const UserRoutes_1 = tslib_1.__importDefault(require("@/routers/UserRoutes"));
 const ImageRoutes_1 = tslib_1.__importDefault(require("@/routers/ImageRoutes"));
@@ -17,7 +19,25 @@ const PostRoutes_1 = tslib_1.__importDefault(require("@/routers/PostRoutes"));
 const app = (0, express_1.default)();
 app.use(express_1.default.json());
 app.use(express_1.default.urlencoded({ extended: false }));
-app.use(cors_1.default);
+const whitelist = [
+    "http://localhost:3000",
+    ...(((_a = process.env.ALLOW_DOMAIN) === null || _a === void 0 ? void 0 : _a.split(", ")) || []),
+];
+const corsOptions = {
+    origin: function (origin, callback) {
+        if (!origin || whitelist.indexOf(origin) !== -1) {
+            callback(null, true);
+        }
+        else {
+            callback(new Error("Not allowed by CORS"));
+        }
+    },
+    methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
+    credentials: true,
+    preflightContinue: false,
+    optionsSuccessStatus: 204,
+};
+app.use((0, cors_1.default)(corsOptions));
 app.use((0, helmet_1.default)());
 app.use("/api/user", UserRoutes_1.default);
 app.use("/api/post", PostRoutes_1.default);
--- src/index.ts
+++ src/index.ts
@@ -2,11 +2,12 @@
 import express from "express";
 import dotenv from "dotenv";
 import helmet from "helmet";
+import cors from "cors";
 dotenv.config();
 if (process.env.NODE_ENV === "production") {
   require("module-alias/register");
 }
-import cors from "@/middleware/cors";
+// import cors from "@/middleware/cors";
 import mongodbConfig from "@/lib/mongodb/config";
 import userRoutes from "@/routers/UserRoutes";
 import imageRoutes from "@/routers/ImageRoutes";
@@ -16,7 +17,27 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
-app.use(cors);
+const whitelist = [
+  "http://localhost:3000",
+  ...(process.env.ALLOW_DOMAIN?.split(", ") || []),
+];
+const corsOptions = {
+  origin: function (
+    origin: string | undefined,
+    callback: (err: Error | null, allow?: boolean) => void
+  ) {
+    if (!origin || whitelist.indexOf(origin) !== -1) {
+      callback(null, true);
+    } else {
+      callback(new Error("Not allowed by CORS"));
+    }
+  },
+  methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
+  credentials: true,
+  preflightContinue: false,
+  optionsSuccessStatus: 204,
+};
+app.use(cors(corsOptions));
 app.use(helmet());
 
 app.use("/api/user", userRoutes);
